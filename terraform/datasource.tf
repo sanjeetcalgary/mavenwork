cb281b69569fb07f40e2c2fef440228102f7c931
sanjeet kumar <sanjeetcalgary@gmail.com>
2023-07-08 11:08:00 -0600

    new

--- terraform/datasource.tf
+++ terraform/datasource.tf
@@ -1,6 +1,11 @@
 data "aws_ami" "amzami" {
   most_recent = true
   owners = [ "amazon", "self" ]
+
+    filter {
+        name = "name"
+        values = [ "Amazon Linux 2023 *" ]
+    }
   
     filter {
         name = "root-device-type"
